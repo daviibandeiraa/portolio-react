95866929346fa74e07f132905beb901d6e5c701d
Davi Barbosa Bandeira <daviibandeiraa@gmail.com>
2024-07-16 20:00:39 -0300

    update social medias

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
         <img className="w-16" src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl"> 
-        <FaLinkedin className="cursor-pointer hover:text-violet-600" target="_blank" href="https://www.linkedin.com/in/davibandeira/"/>
-        <FaGithub className="cursor-pointer hover:text-violet-600" target="_blank" href="https://github.com/daviibandeiraa" />
-        <FaInstagram className="cursor-pointer hover:text-violet-600" target="_blank" href="https://instagram.com/bandeiraa_04"/>
-        <FaXTwitter className="cursor-pointer hover:text-violet-600" target="_blank" href="https://x.com/daviibandeiraa" />
+        <a href="https://www.linkedin.com/in/davibandeira/"><FaLinkedin className="cursor-pointer hover:text-violet-600"/></a>
+        <a href="https://www.github.com/in/daviibandeiraa/"><FaGithub className="cursor-pointer hover:text-violet-600"/></a>
+        <a href="https://instagram.com/bandeiraa_04"><FaInstagram className="cursor-pointer hover:text-violet-600"></FaInstagram></a>
+        <a href="https://x.com/daviibandeiraa">  <FaXTwitter className="cursor-pointer hover:text-violet-600"></FaXTwitter></a>
     </div>
   </nav>
 }
